1632c8288e5deff0cc394ef53cb26a72e277a872
Empyrean-Void <jshortz@proton.me>
2023-08-31 17:22:48 -0600

    Changed default parsers and language servers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'lua', 'python' },
+  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'markdown' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,

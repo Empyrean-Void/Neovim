83fe2563a4889ba3ba2a814cbfb6fa657aeb457f
Empyrean-Void <jshortz@proton.me>
2023-09-02 20:36:26 -0600

    Fixed tab size

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -19,6 +19,11 @@ vim.o.clipboard = 'unnamedplus'
 -- Enable break indent
 vim.o.breakindent = true
 
+-- Tabs
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.opt.expandtab = true
+
 -- Save undo history
 vim.o.undofile = true
 
